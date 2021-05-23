d45f1828249e80202ca09b2c88b34cb6ded4cc3c
agata.rubacka <agata.rubacka@solactive.com>
2021-05-23 18:07:10 +0200

    Add StandardizerView

--- standardizing_api/views.py
+++ standardizing_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.request import Request
+
+from standardizing_api.serializers import RequestBodySerializer
+
+class StandardizerView(viewsets.ViewSet):
+
+    def supervise_standardization(self, request: Request):
+
+        serializer = RequestBodySerializer(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            print(validated_data)
+            return HttpResponse({'data':'is_there'})
+
+standardizer_view = StandardizerView.as_view({"post": "supervise_standardization"})
