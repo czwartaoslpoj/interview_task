a48c9d7a7e66ac70707176a1821544015d4c49b7
agata.rubacka <agata.rubacka@solactive.com>
2021-05-23 20:53:25 +0200

    Add client and json payloads fixtures

--- standardizing_api/tests/conftest.py
+++ standardizing_api/tests/conftest.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 import numpy as np
 import pytest
+from rest_framework.test import APIClient
 
 from standardizing_api.transformer import Transformer
 
@@ -19,6 +20,7 @@ def validated_data():
                                    "sensor_3": [0.44, 0.22, 0.55, 0.54, 0.24]}))
     return validated_data
 
+
 @pytest.fixture
 def combined_list():
     combined_list = [[5.44, 3.22, 6.55, 8.54, 1.24],
@@ -33,3 +35,25 @@ def transformed_list():
                                  [1.96026147, -0.82000937, -0.51721314, -0.41959677, -0.2034422],
                                  [0.29452117, -1.24820879, 1.06588616, 0.99576207, -1.10796061]])
     return transformed_list
+
+
+@pytest.fixture(scope="session")
+def client():
+    client = APIClient()
+    return client
+
+
+@pytest.fixture(scope="session")
+def json_payload():
+    json_payload = {"sensor_1": [5.44, 3.22, 6.55, 8.54, 1.24],
+                    "sensor_2": [5444.44, 33.22, 622.55, 812.54, 1233.24],
+                    "sensor_3": [0.44, 0.22, 0.55, 0.54, 0.24]}
+    return json_payload
+
+
+@pytest.fixture(scope="session")
+def incorrect_json_payload(request):
+    incorrect_json_payload = {"sensor_1": request.param,
+                              "sensor_2": [5444.44, 33.22, 622.55, 812.54, 1233.24],
+                              "sensor_3": [0.44, 0.22, 0.54, 0.24]}
+    return incorrect_json_payload
