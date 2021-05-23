a53ca7ab243b4fdb8feb74fac26c4e7fc5c87add
agata.rubacka <agata.rubacka@solactive.com>
2021-05-23 19:48:53 +0200

    Fix supervise_standardization method

--- standardizing_api/views.py
+++ standardizing_api/views.py
@@ -16,7 +16,11 @@ class StandardizerView(viewsets.ViewSet):
         serializer = RequestBodySerializer(data=request.data)
         if serializer.is_valid():
             validated_data = serializer.validated_data
-            print(validated_data)
-            return HttpResponse({'data':'is_there'})
+            transformed_data = self.transformer.transform(validated_data)
+            print(transformed_data.as_dict())
+            return HttpResponse(json.dumps(transformed_data.as_dict()), content_type="application/json")
+        else:
+            return HttpResponse(serializer.errors, status=400)
+
 
 standardizer_view = StandardizerView.as_view({"post": "supervise_standardization"})
