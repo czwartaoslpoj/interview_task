5ef1dadf5a22ff64fcc9395ed4f656e9fa1b7da2
agata.rubacka <agata.rubacka@solactive.com>
2021-05-23 17:06:57 +0200

    Add validate method to RequestBodySerializer

--- standardizing_api/standardizing_api/seralizers.py
+++ standardizing_api/standardizing_api/seralizers.py
@@ -5,3 +5,11 @@ class RequestBodySerializer(serializers.Serializer):
     sensor_1 = serializers.ListField(child=serializers.DecimalField(min_value=0.0, max_digits=20, decimal_places=2))
     sensor_2 = serializers.ListField(child=serializers.DecimalField(min_value=0.0, max_digits=20, decimal_places=2))
     sensor_3 = serializers.ListField(child=serializers.DecimalField(min_value=0.0, max_digits=20, decimal_places=2))
+
+    def validate(self, data):
+        if len(data["sensor_1"]) == len(data["sensor_2"]) == len(data["sensor_3"]):
+            return data
+        else:
+            raise serializers.ValidationError("All sensors lists must be of the same size")
+    class Meta:
+        fields = ['sensor_1', 'sensor_2', 'sensor_3']
