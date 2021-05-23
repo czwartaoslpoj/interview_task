7bbfe588e7cf2f9f6c456d800e49afbc7dfe2e29
agata.rubacka <agata.rubacka@solactive.com>
2021-05-23 19:32:21 +0200

    Add as_dict methods to dataclasses

--- standardizing_api/models.py
+++ standardizing_api/models.py
@@ -7,12 +7,16 @@ class SensorResults:
     sensor2: list
     sensor3: list
 
+    def as_dict(self):
+        return {'sensor1': self.sensor1, 'sensor2': self.sensor2, 'sensor3': self.sensor3}
 
 @dataclass
 class StandardizeResponseBody:
     success: bool
     result: SensorResults
 
+    def as_dict(self):
+        return {'success': self.success, 'result': self.result.as_dict()}
 
 class ParsingError(Exception):
     def __init__(self, message):
