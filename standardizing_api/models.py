876f78543e76c19b65184a3013038015c06050b6
agata.rubacka <agata.rubacka@solactive.com>
2021-05-23 19:32:53 +0200

    Format models.py

--- standardizing_api/models.py
+++ standardizing_api/models.py
@@ -10,6 +10,7 @@ class SensorResults:
     def as_dict(self):
         return {'sensor1': self.sensor1, 'sensor2': self.sensor2, 'sensor3': self.sensor3}
 
+
 @dataclass
 class StandardizeResponseBody:
     success: bool
@@ -18,9 +19,11 @@ class StandardizeResponseBody:
     def as_dict(self):
         return {'success': self.success, 'result': self.result.as_dict()}
 
+
 class ParsingError(Exception):
     def __init__(self, message):
         self.message = message
+
 
 class ScalingError(Exception):
     def __init__(self, message):
